915323b01092f31524899a3c9e550fc731027839
Sirajum Munira <muniramujaris975@gmail.com>
2025-06-12 23:22:36 +0200

    Added middleware to mount the routes.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import dbConnect from './db.js';
+import employeeRoutes from './routes/employees.js';
+import projectRoutes from './routes/projects.js';
+import assignmentRoutes from './routes/project_assignments.js';
 
 dotenv.config({ path: './.env' });
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+app.use('/api/employees', employeeRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/project_assignments', assignmentRoutes);
 
 
 const PORT = process.env.PORT;
